1c5c841fc4022733b09a10baf745417f5b822fbc
Omar Estoque <mgestoquejr@gmail.com>
2020-09-15 14:27:18 -0700

    Fill in methods with passing functions

--- lib/code_challenge.rb
+++ lib/code_challenge.rb
@@ -8,33 +8,50 @@
 class CodeChallenge
   # Write a function that determines if a string starts with an upper-case letter A-Z.
   def start_with_uppercase_letter?(string)
-    # Fill In
+    !!(string =~ /^[A-Z]/)
   end
 
   # Write a function that determines the area of a circle given the radius.
   def area_of_circle(radius)
-    # Fill In
+    return 'radius should be positive' unless radius.to_f > 0
+    Math::PI * (radius**2)
   end
 
   # Write a function to add up all the values in an array.
   def sum_array(array)
-    # Fill In
+    array.inject(0) { |sum,x| sum += x }
   end
 
   # Write a function that determines if a string is a palindrome.
   def palindrome?(string)
-    # Fill In
+    string.downcase == string.reverse.downcase
   end
 
   # Write a recursive function to determine the Nth Fibonacci Number in the Fibonacci sequence
   def nth_fibonacci(nth)
-    # Fill In
+    fib = [0,1]
+    counter = 0
+
+    while counter < nth do
+      next_num = fib.shift + fib.last
+      fib.push(next_num)
+      counter += 1
+    end
+
+    fib.first
   end
 
   # Write a function that takes and array of numbers, multiplies each them by their index, and writes it's values to a csv.
   # The csv should have have 3 columns. index, number, and sum.
   def write_array_to_csv(array)
     filename = 'test.csv' # test expects this filename
-    # Fill In
+
+    File.open(filename, 'wb') do |f|
+      f.write("index,number,sum\n")
+
+      array.each.with_index do |x,index|
+        f.write("#{index},#{x},#{index * x}\n")
+      end
+    end
   end
 end
